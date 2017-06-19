761d8782422f50993589a17aa6f3fad70a2484ff
Tao Wen <taowen@gmail.com>
2017-06-19 15:44:58 +0800

    #2 implement update entity

--- pstore.go
+++ pstore.go
@@ -4,6 +4,7 @@ import (
 	"github.com/v2pro/psql"
 	"time"
 	"github.com/json-iterator/go"
+	"fmt"
 )
 
 type entityStore struct {
@@ -11,22 +12,41 @@ type entityStore struct {
 	getLatestStateSql *psql.TranslatedSql
 	getEventSql       *psql.TranslatedSql
 	jsonApi           jsoniter.Api
+	commandHandlers   map[string]HandleCommand
 }
 
 func StoreOf(entityName string) *entityStore {
 	insertSql := psql.Translate(
 		"INSERT "+entityName+" :INSERT_COLUMNS",
-		"entity_id", "event_id", "event_name", "command", "state")
+		"entity_id", "event_id", "command_id", "command_name", "command_body", "state")
 	getLatestStateSql := psql.Translate(
-		"SELECT * FROM " + entityName + " WHERE entity_id=:entity_id ORDER BY event_id LIMIT 1")
+		"SELECT * FROM " + entityName + " WHERE entity_id=:entity_id ORDER BY event_id DESC LIMIT 1")
 	getEventSql := psql.Translate(
-		"SELECT * FROM " + entityName + " WHERE entity_id=:entity_id AND event_name=:event_name")
+		"SELECT * FROM " + entityName + " WHERE entity_id=:entity_id AND command_id=:command_id")
 	return &entityStore{
 		insertSql:         insertSql,
 		getLatestStateSql: getLatestStateSql,
 		getEventSql:       getEventSql,
 		jsonApi:           jsoniter.ConfigDefault,
+		commandHandlers:   map[string]HandleCommand{},
 	}
+}
+
+func (store *entityStore) JsonApi(jsonApi jsoniter.Api) *entityStore {
+	store.jsonApi = jsonApi
+	return store
+}
+
+type Command struct {
+	CommandName string
+	CommandId   string
+}
+
+type HandleCommand func(commandName string, commandId string, commandBody interface{}, state jsoniter.Any) (interface{}, error)
+
+func (store *entityStore) Command(commandName string, handleCommand HandleCommand) *entityStore {
+	store.commandHandlers[commandName] = handleCommand
+	return store
 }
 
 type Entity struct {
@@ -36,28 +56,30 @@ type Entity struct {
 	UpdatedAt time.Time
 }
 
-func (store *entityStore) Create(conn *psql.Conn, entityId string, dataObj interface{}) error {
-	data, err := store.jsonApi.MarshalToString(dataObj)
+func (store *entityStore) Create(conn *psql.Conn, entityId string, state interface{}) error {
+	stateJson, err := store.jsonApi.MarshalToString(state)
 	if err != nil {
 		return err
 	}
 	_, insertErr := conn.Exec(store.insertSql,
 		"entity_id", entityId,
 		"event_id", int64(1),
-		"event_name", "created",
-		"command", data,
-		"state", data)
+		"command_id", "create",
+		"command_name", "create",
+		"command_body", stateJson,
+		"state", stateJson)
 	if insertErr == nil {
 		return nil
 	}
 	stmt := conn.Statement(store.getEventSql)
 	defer stmt.Close()
-	rows, err := stmt.Query("entity_id", entityId, "event_name", "created")
+	rows, err := stmt.Query("entity_id", entityId, "command_id", "create")
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
-	if rows.Next() == nil {
+	err = rows.Next()
+	if err == nil {
 		return nil
 	}
 	return insertErr
@@ -79,7 +101,51 @@ func (store *entityStore) Get(conn *psql.Conn, entityId string) (*Entity, error)
 		EntityId:  entityId,
 		Version:   rows.GetInt64(rows.C("event_id")),
 		State:     store.jsonApi.Get([]byte(rows.GetString(rows.C("state")))),
-		UpdatedAt: rows.GetTime(rows.C("created_at")),
+		UpdatedAt: rows.GetTime(rows.C("handled_at")),
 	}
 	return entity, nil
+}
+
+func (store *entityStore) Update(conn *psql.Conn, entityId string, commandId string, commandName string, commandBody interface{}) error {
+	handleCommand := store.commandHandlers[commandName]
+	if handleCommand == nil {
+		return fmt.Errorf("no handler defined for command: %v", commandName)
+	}
+	entity, err := store.Get(conn, entityId)
+	if err != nil {
+		return err
+	}
+	newState, err := handleCommand(commandName, commandId, commandBody, entity.State)
+	if err != nil {
+		return err
+	}
+	commandBodyJson, err := store.jsonApi.MarshalToString(commandBody)
+	if err != nil {
+		return err
+	}
+	stateJson, err := store.jsonApi.MarshalToString(newState)
+	if err != nil {
+		return err
+	}
+	_, insertErr := conn.Exec(store.insertSql,
+		"entity_id", entityId,
+		"event_id", entity.Version+1,
+		"command_id", commandId,
+		"command_name", commandName,
+		"command_body", commandBodyJson,
+		"state", stateJson)
+	if insertErr != nil {
+		stmt := conn.Statement(store.getEventSql)
+		defer stmt.Close()
+		rows, err := stmt.Query("entity_id", entityId, "command_id", commandId)
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+		err = rows.Next()
+		if err == nil {
+			return nil
+		}
+	}
+	return insertErr
 }
--- pstore_test.go
+++ pstore_test.go
@@ -5,9 +5,32 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/v2pro/psql"
 	"github.com/json-iterator/go/require"
+	"github.com/json-iterator/go"
+	"fmt"
 )
 
-var accounts = StoreOf("account")
+type Account struct {
+	UsableBalance int64
+	FrozenBalance int64
+}
+
+var accounts = StoreOf("account").
+	JsonApi(jsoniter.ConfigDefault).
+	Command("transfer1pc",
+	func(commandName string, commandId string, commandBody interface{}, state jsoniter.Any) (interface{}, error) {
+		amount := commandBody.(int64)
+		account := Account{}
+		state.ToVal(&account)
+		if state.LastError() != nil {
+			return nil, state.LastError()
+		}
+		oldBalance := account.UsableBalance
+		account.UsableBalance += amount
+		if account.UsableBalance < 0 {
+			return nil, fmt.Errorf("account balance can not be negative: %v => %v", oldBalance, account.UsableBalance)
+		}
+		return account, nil
+	})
 
 func Test_create(t *testing.T) {
 	should := require.New(t)
@@ -31,4 +54,41 @@ func Test_create_should_be_idempotent(t *testing.T) {
 	accountId := NewID().String()
 	should.Nil(accounts.Create(conn, accountId, nil))
 	should.Nil(accounts.Create(conn, accountId, nil))
+}
+
+func Test_update(t *testing.T) {
+	should := require.New(t)
+	drv := mysql.MySQLDriver{}
+	conn, err := psql.Open(drv, "root:123456@tcp(127.0.0.1:3306)/v2pro")
+	should.Nil(err)
+	defer conn.Close()
+	accountId := NewID().String()
+	should.Nil(accounts.Create(conn, accountId, &Account{}))
+	should.Nil(accounts.Update(conn, accountId, "xxx-001", "transfer1pc", int64(100)))
+	account, err := accounts.Get(conn, accountId)
+	should.Nil(err)
+	should.Equal(100, account.State.Get("UsableBalance").ToInt())
+}
+
+func Test_update_should_be_idempotent(t *testing.T) {
+	should := require.New(t)
+	drv := mysql.MySQLDriver{}
+	conn, err := psql.Open(drv, "root:123456@tcp(127.0.0.1:3306)/v2pro")
+	should.Nil(err)
+	defer conn.Close()
+	accountId := NewID().String()
+	should.Nil(accounts.Create(conn, accountId, &Account{}))
+	should.Nil(accounts.Update(conn, accountId, "xxx-001", "transfer1pc", int64(100)))
+	should.Nil(accounts.Update(conn, accountId, "xxx-001", "transfer1pc", int64(100)))
+}
+
+func Test_update_should_not_violate_command_constraint(t *testing.T) {
+	should := require.New(t)
+	drv := mysql.MySQLDriver{}
+	conn, err := psql.Open(drv, "root:123456@tcp(127.0.0.1:3306)/v2pro")
+	should.Nil(err)
+	defer conn.Close()
+	accountId := NewID().String()
+	should.Nil(accounts.Create(conn, accountId, &Account{}))
+	should.NotNil(accounts.Update(conn, accountId, "xxx-001", "transfer1pc", int64(-100)))
 }
